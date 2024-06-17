9edc1361581bcb7816dce4486a1503839c1c89ce
Arpan Sethi <arpansethi30@gmail.com>
2024-06-17 19:01:27 +0530

    app

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,14 +1,18 @@
 import os
 import sys
-import pandas as pd
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
 from src.exception import CustomException
 from src.logger import logging
+import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-from data_transformation import DataTransformation
-from data_transformation import DataTransformationConfig
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
 
@@ -27,7 +31,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv("notebook/data/stud.csv")
+            df = pd.read_csv("notebook\data\stud.csv")
             logging.info("Read the dataset as dataframe")
 
             os.makedirs(
--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,25 @@
 import os
 import sys
-import pandas as pd
+
 import numpy as np
+import pandas as pd
 import dill
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
 
 
 def save_object(file_path, obj):
-    """
-    This function is used to save the data
-    """
     try:
         dir_path = os.path.dirname(file_path)
+
         os.makedirs(dir_path, exist_ok=True)
+
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
 
@@ -49,6 +51,15 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
